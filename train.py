ddfe4a67de959c15a1ffab8fc00bb460b5699953
Li Yachao <harry_lyc@foxmail.com>
2019-04-13 16:35:27 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,57 @@
+import numpy
+import os
+import time
+
+
+from nmt import train
+
+def main(job_id, params):
+    print ('timestamp {} {}'.format('running',time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    print (params)
+    validerr = train(saveto=params['model'][0],
+                     reload_=params['reload'][0],
+                     dim_word=params['dim_word'][0],
+                     dim=params['dim'][0],
+                     n_words=params['n-words'][0],
+                     n_words_src=params['n-words'][0],
+                     decay_c=params['decay-c'][0],
+                     clip_c=params['clip-c'][0],
+                     lrate=params['learning-rate'][0],
+                     optimizer=params['optimizer'][0],
+                     patience=1000,
+                     maxlen=50,
+                     batch_size=80,
+                     validFreq_fine=2000,
+                     validFreq=10000,
+                     val_burn_in=20000,
+                     val_burn_in_fine=80000,
+                     dispFreq=20,
+                     saveFreq=2000,
+                     sampleFreq=200,
+                     datasets=['/home/ycli/resource/hw/ch.txt.shuffle',
+                               '/home/ycli/resource/hw/en.txt.shuffle'],
+                     valid_datasets=['/home/ycli/resource/hw/valid/valid_src',
+                                     '/home/ycli/resource/hw/valid/valid_trg',
+                                     './data/valid_out'],
+                     dictionaries=['/home/ycli/resource/hw/vocab/vocab_src.pkl',
+                                   '/home/ycli/resource/hw/vocab/vocab_trg.pkl'],
+                     use_dropout=params['use-dropout'][0],
+                     overwrite=False,
+                     valid_mode=params['valid_mode'][0],
+                     bleu_script=params['bleu_script'][0])
+    return validerr
+
+if __name__ == '__main__':
+    main(0, {
+        'model': ['/data/ycli/exp/gate-nmt/all/tmp/train_model.npz'],
+        'dim_word': [620],
+        'dim': [1000],
+        'n-words': [30001],
+        'optimizer': ['sgd'],
+        'decay-c': [0.],
+        'clip-c': [1.],
+        'use-dropout': [True],
+        'learning-rate': [2.],
+        'reload': [False],
+        'valid_mode': ['bleu', 'ce'],
+        'bleu_script': ['./data/mteval-v11b.pl', './data/multi-bleu.perl']})
---
+++ translate.py
@@ -0,0 +1,93 @@
+'''
+Translates a source file using a translation model.
+'''
+import argparse
+import subprocess
+import numpy
+import time
+import sys
+import cPickle as pkl
+import os
+
+from nmt import (build_sampler, load_params, init_params, init_tparams)
+from nmt import _idxs2words, gen_trans
+from stream import get_sentences
+
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
+from theano import shared
+import pdb
+
+
+def sample(model, dictionary, dictionary_target, \
+           source_file, ref_file, saveto, \
+           k=10, normalize=False, \
+           bleu_script='./data/multi-bleu.perl', res_to_sgm='./data/plain2sgm'):
+
+    # load model model_options
+    with open(model+'.pkl', 'rb') as f:
+        options = pkl.load(f)
+
+    # load target dictionary and invert
+    with open(dictionary_target, 'rb') as f:
+        word_dict_trg = pkl.load(f)
+    word_idict_trg = dict()
+    for kk, vv in word_dict_trg.iteritems():
+        word_idict_trg[vv] = kk
+
+    val_start_time = time.time()
+
+    trng = RandomStreams(1234)
+    use_noise = shared(numpy.float32(0.))
+
+    # allocate model parameters
+    params = init_params(options)
+
+    # load model parameters and set theano shared variables
+    params = load_params(model, params)
+    tparams = init_tparams(params)
+    # word index
+    f_init, f_next = build_sampler(tparams, options, trng, use_noise)
+
+    bleu_score = gen_trans(test_src=source_file, test_ref=ref_file, out_file=saveto, \
+                           dict_src=dictionary, idict_trg=word_idict_trg, \
+                           tparams=tparams, f_init=f_init, f_next=f_next, model_options=options, \
+                           trng=trng, k=10, stochastic=False, bleu_script=bleu_script, res_to_sgm=res_to_sgm, apply_bpe=True)
+
+    print(model+' / '+source_file+' / '+'test bleu %.4f' %bleu_score)
+    print ('timestamp {} {}'.format('done',time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    sys.stdout.flush()
+    
+if __name__ == "__main__":
+    '''
+
+    '''
+    print ('timestamp {} {}'.format('running',time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+
+    dict_src = '/data/ycli/resource/wmt2017/deen/vocab/v30-bpe/vocab_en.pkl'
+    dict_trg = '/data/ycli/resource/wmt2017/deen/vocab/v30-bpe/vocab_de.pkl'
+
+    test_model = ['/data/ycli/exp/deen/bb/train_model.iter200000.npz',
+                  '/data/ycli/exp/deen/bb/train_model.itertt152000.npz',
+                  '/data/ycli/exp/deen/bb/train_model.itertt1108000.npz']
+
+    test_file = [['/data/ycli/resource/wmt2017/deen/dev/newstest2014.tc.en.bpe',
+                 '/data/ycli/resource/wmt2017/deen/dev/newstest2014.tc.de',
+                 './data/news2014.out'],
+                 ['/data/ycli/resource/wmt2017/deen/dev/newstest2015.tc.en.bpe',
+                 '/data/ycli/resource/wmt2017/deen/dev/newstest2015.tc.de',
+                 './data/news2015.out'],
+                 ['/data/ycli/resource/wmt2017/deen/dev/newstest2016.tc.en.bpe',
+                 '/data/ycli/resource/wmt2017/deen/dev/newstest2016.tc.de',
+                 './data/news2016.out']]
+
+    for model in test_model:
+        
+        if os.path.isfile(model) == False:
+            #print('file [{}] do not exist'.format(model))
+            continue
+        for i in range(len(test_file)):
+            if os.path.isfile(test_file[i][0]) == False:
+                #print('file [{}] do not exist'.format(test_file[i][0]))
+                continue
+            sample(model, dict_src, dict_trg, \
+	               test_file[i][0], test_file[i][1], test_file[i][2])
